02c5ab988589edf9f0e14303337b65c0867e8532
Elias Wambugu <e.wambugu192@gmail.com>
2024-05-21 18:48:55 -0700

    go to app dashboard when clicking on logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import { FileText } from "lucide-react";
 import Link from "next/link";
+import { useUser } from "@clerk/clerk-react";
 
 export default function Logo({size}: {size?: number | string}) {
+  const { isSignedIn, user, isLoaded } = useUser();
+
   return (
     <div className="flex flex-row">
       <FileText />
-      <Link href='/' className='font-bold text-lg ml-2 hover:cursor-pointer'>myresume.guru</Link>
+      <Link href={user ? '/app/resumes' : '/'} className='font-bold text-lg ml-2 hover:cursor-pointer'>myresume.guru</Link>
     </div>
   );
 }
