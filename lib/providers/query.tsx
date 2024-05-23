a2da1594507dbbc6130ed8493b7be681cb7718cb
Elias Wambugu <e.wambugu192@gmail.com>
2024-05-22 21:11:33 -0700

    setup react query

---
+++ lib/providers/query.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
+
+export default function Query({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}<ReactQueryDevtools initialIsOpen={false} /></QueryClientProvider>
+  );
+}
