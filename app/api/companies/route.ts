74d9254c34f79d7ae636c511f1ca444dc76babe9
Elias Wambugu <e.wambugu192@gmail.com>
2024-05-31 02:11:53 -0700

    added api routes for page/companies

---
+++ app/api/companies/route.ts
@@ -0,0 +1,26 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function GET() {
+  const companies = await prisma.company.findMany({
+    include: { applications: true },
+  });
+  return NextResponse.json(companies);
+}
+
+export async function POST(req: NextRequest) {
+  const user = await currentUser();
+
+  const { name, website } = await req.json();
+
+  await prisma.company.create({
+    data: {
+      name,
+      website,
+      userId: user!.id,
+    },
+  });
+
+  return NextResponse.json({ success: true });
+}
