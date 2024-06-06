7964042f61058abb65b2eaa0b73f81adcbfc65ac
Elias Wambugu <e.wambugu192@gmail.com>
2024-06-06 12:20:36 -0700

    redid logic for application title component

--- components/applications/applicationTitle.tsx
+++ components/applications/applicationTitle.tsx
@@ -26,20 +26,6 @@ export default function ApplicationTitle({ id }: { id: string }) {
           </h1>
         </>
       )}
-      {/* {status === "Loading" ? (
-        <>
-          <h1 className="mt-[-6px] font-light  text-2xl text-black">
-            {status === "Loading" && !application ? "Loading..." : application?.title}
-          </h1>
-          <Badge className="ml-2  self-center">Active</Badge>
-        </>
-      ) : (
-        <>
-          <h1 className="mt-[-6px] font-light  text-2xl text-black">
-            Loading...
-          </h1>
-        </>
-      )} */}
     </>
   );
 }
