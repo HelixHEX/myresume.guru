9db8ba87e7a6f618ee0e39269718e96bb58d06b4
Elias Wambugu <e.wambugu192@gmail.com>
2024-06-07 12:47:32 -0700

    temporarily remove auth from main app

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references /nextjs/auth-middleware for more information about configuring your Middleware
 
-const isProtectedRouteClient = createRouteMatcher(["/app(.*)", "/api(.*)"]);
+const isProtectedRouteClient = createRouteMatcher(["/app(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRouteClient(req) && !req.url.includes("/api/uploadthing"))
