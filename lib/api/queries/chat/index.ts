be6c8f48435ba6493ccbf9c0a7c2a34689c294ac
Elias Wambugu <e.wambugu192@gmail.com>
2024-06-15 14:33:22 -0700

    fixed all errors

--- lib/api/queries/chat/index.ts
+++ lib/api/queries/chat/index.ts
@@ -10,7 +10,7 @@ export const useGetMessages = ({
   resumeId,
   enabled = false,
 }: {
-  resumeId: number;
+  resumeId: Resume['id'];
   enabled: boolean;
 }) => {
   return useQuery({
--- lib/context/resume/index.tsx
+++ lib/context/resume/index.tsx
@@ -5,34 +5,205 @@ import { api } from "@/lib/api";
 import { useChat } from "ai/react";
 import React, { createContext, useContext, useState } from "react";
 
+interface LayoutContextProps {
+  sortBy: string;
+  setSortBy: React.Dispatch<React.SetStateAction<string>>;
+  resume: Resume | null;
+  setResume: React.Dispatch<React.SetStateAction<Resume | null>>;
+  status: Feedback["status"];
+  setStatus: React.Dispatch<React.SetStateAction<Feedback["status"]>>;
+  feedbacks: Feedback[];
+  setFeedbacks: React.Dispatch<React.SetStateAction<Feedback[]>>;
+}
+
+export const LayoutContext = createContext<LayoutContextProps>({
+  sortBy: "",
+  setSortBy: () => {},
+  resume: null,
+  setResume: () => {},
+  status: "loading",
+  setStatus: () => {},
+  feedbacks: [],
+  setFeedbacks: () => {},
+});
+
+export function LayoutProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [resume, setResume] = useState<Resume | null>(null);
+  const [status, setStatus] = useState<Feedback["status"]>("loading");
+  const [sortBy, setSortBy] = useState<string>("");
+  const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
+
+  return (
+    <LayoutContext.Provider
+      value={{
+        sortBy,
+        setSortBy,
+        status,
+        setStatus,
+        resume,
+        setResume,
+        feedbacks,
+        setFeedbacks,
+      }}
+    >
+      {children}
+    </LayoutContext.Provider>
+  );
+}
+
+export const useInitiateAssistantUI = () => {
+  const { resume, feedbacks } = useContext(LayoutContext);
+
+  const { data } = api.queries.chat.useGetMessages({
+    resumeId: resume!.id,
+    enabled: resume!.id !== null,
+  });
+  const chat = useChat({
+    api: "/api/ai/chat",
+    id: "chat",
+    // initialMessages: data?.map((m) => ({
+    //   id: m.id.toString(),
+    //   content: m.content,
+    //   role: m.role as "user" | "assistant" | "system",
+    // })),
+
+    // onFinish: (message) => {
+    //   saveMessageToDb({
+    //     message,
+    //     resumeId: resume?.id,
+    //     applicationId: resume?.applicationId,
+    //     userId: resume?.userId,
+    //   });
+    // },
+    body: {
+      context: [
+        {
+          role: "system",
+          content:
+            "You area resume analyzer tool. You will be analyzing a user-uploaded resume that has been converted to plain text. You also have already provided some feedback on the resume. Your job is to answer any questions the user has about their resume or the feedback provided",
+        },
+        // {
+        //   role: "system",
+        //   content: `resume: ${resume?.text}`,
+        // },
+        // {
+        //   role: "system",
+        //   content: `feedback you have already provided. Use it as context for responding to any questions users have: ${feedbacks
+        //     .map((f: Feedback, index) => {
+        //       return `\n- ${f.title}: ${f.text ?? ""} \n${f.actionableFeedbacks
+        //         ?.map(
+        //           (aF, aFIndex) =>
+        //             `${aFIndex + 1}. ${aF.title}: ${aF.text ?? ""}`
+        //         )
+        //         .join("\n")}`;
+        //     })
+        //     .join("")}`,
+        // },
+      ],
+      resumeId: resume!.id,
+      // applicationId: resume!.applicationId,
+      userId: resume!.userId,
+    },
+  });
+
+  return { chat };
+};
+
 // interface LayoutContextProps {
 //   sortBy: string;
 //   setSortBy: React.Dispatch<React.SetStateAction<string>>;
-//   resume: Resume | null;
-//   setResume: React.Dispatch<React.SetStateAction<Resume | null>>;
-//   status: Feedback["status"];
-//   setStatus: React.Dispatch<React.SetStateAction<Feedback["status"]>>;
-//   feedbacks: Feedback[];
+//   resume: any | null;
+//   setResume: React.Dispatch<React.SetStateAction<any>>;
+//   status: "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database";
+//   setStatus: React.Dispatch<
+//     React.SetStateAction<
+//       "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database"
+//     >
+//   >;
+//   feedbacks: Feedback[],
 //   setFeedbacks: React.Dispatch<React.SetStateAction<Feedback[]>>;
 // }
 
-// export const LayoutContext = createContext<LayoutContextProps>({
+// const LayoutContext = createContext<LayoutContextProps>({
 //   sortBy: "",
 //   setSortBy: () => {},
 //   resume: null,
 //   setResume: () => {},
-//   status: "loading",
+//   status: "Loading",
 //   setStatus: () => {},
 //   feedbacks: [],
 //   setFeedbacks: () => {},
 // });
 
-// export default function LayoutProvider({children}: {children: React.ReactNode}) {
+// export const useInitiateAssistantUI = () => {
+//   const { resume, feedbacks } = useContext(LayoutContext);
+
+// const { data } = api.queries.chat.useGetMessages({
+//   resumeId: resume?.id,
+//   enabled: resume?.id !== null,
+// });
+
+// const chat = useChat({
+//   api: "/api/ai/chat",
+//   id: "chat",
+//   // initialMessages: data?.map((m) => ({
+//   //   id: m.id.toString(),
+//   //   content: m.content,
+//   //   role: m.role as "user" | "assistant" | "system",
+//   // })),
+
+//   // onFinish: (message) => {
+//   //   saveMessageToDb({
+//   //     message,
+//   //     resumeId: resume?.id,
+//   //     applicationId: resume?.applicationId,
+//   //     userId: resume?.userId,
+//   //   });
+//   // },
+//   body: {
+//     context: [
+//       {
+//         role: "system",
+//         content:
+//           "You area resume analyzer tool. You will be analyzing a user-uploaded resume that has been converted to plain text. You also have already provided some feedback on the resume. Your job is to answer any questions the user has about their resume or the feedback provided",
+//       },
+//       // {
+//       //   role: "system",
+//       //   content: `resume: ${resume?.text}`,
+//       // },
+//       // {
+//       //   role: "system",
+//       //   content: `feedback you have already provided. Use it as context for responding to any questions users have: ${feedbacks
+//       //     .map((f: Feedback, index) => {
+//       //       return `\n- ${f.title}: ${f.text ?? ""} \n${f.actionableFeedbacks
+//       //         ?.map(
+//       //           (aF, aFIndex) =>
+//       //             `${aFIndex + 1}. ${aF.title}: ${aF.text ?? ""}`
+//       //         )
+//       //         .join("\n")}`;
+//       //     })
+//       //     .join("")}`,
+//       // },
+//     ],
+//     resumeId: resume?.id,
+//     applicationId: resume?.applicationId,
+//     userId: resume?.userId,
+//   },
+// });
+// return {chat}
+// }
+
+// const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
 //   const [resume, setResume] = useState<Resume | null>(null);
-//   const [status, setStatus] = useState<Feedback["status"]>("loading");
+//   const [status, setStatus] = useState<
+//     "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database"
+//   >("Loading");
 //   const [sortBy, setSortBy] = useState<string>("");
 //   const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
-
 //   return (
 //     <LayoutContext.Provider
 //       value={{ sortBy, setSortBy, status, setStatus, resume, setResume, feedbacks, setFeedbacks }}
@@ -40,122 +211,22 @@ import React, { createContext, useContext, useState } from "react";
 //       {children}
 //     </LayoutContext.Provider>
 //   );
+// };
+
+// /*
+
+// LEGACY CODE
+
+// const ChangeTitle = ({title}: {title: string}) => {
+//   const {setTitle} = useContext(context.resume.LayoutContext)
+
+//   useEffect(() => {
+//     setTitle(title)
+//   }, [title, setTitle]);
+
+//   return null;
 // }
 
-interface LayoutContextProps {
-  sortBy: string;
-  setSortBy: React.Dispatch<React.SetStateAction<string>>;
-  resume: any | null;
-  setResume: React.Dispatch<React.SetStateAction<any>>;
-  status: "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database";
-  setStatus: React.Dispatch<
-    React.SetStateAction<
-      "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database"
-    >
-  >;
-  feedbacks: Feedback[],
-  setFeedbacks: React.Dispatch<React.SetStateAction<Feedback[]>>;
-}
+// */
 
-const LayoutContext = createContext<LayoutContextProps>({
-  sortBy: "",
-  setSortBy: () => {},
-  resume: null,
-  setResume: () => {},
-  status: "Loading",
-  setStatus: () => {},
-  feedbacks: [],
-  setFeedbacks: () => {},
-});
-
-export const useInitiateAssistantUI = () => {
-  const { resume, feedbacks } = useContext(LayoutContext);
-
-const { data } = api.queries.chat.useGetMessages({
-  resumeId: resume?.id,
-  enabled: resume?.id !== null,
-});
-
-const chat = useChat({
-  api: "/api/ai/chat",
-  id: "chat",
-  // initialMessages: data?.map((m) => ({
-  //   id: m.id.toString(),
-  //   content: m.content,
-  //   role: m.role as "user" | "assistant" | "system",
-  // })),
-
-  // onFinish: (message) => {
-  //   saveMessageToDb({
-  //     message,
-  //     resumeId: resume?.id,
-  //     applicationId: resume?.applicationId,
-  //     userId: resume?.userId,
-  //   });
-  // },
-  body: {
-    context: [
-      {
-        role: "system",
-        content:
-          "You area resume analyzer tool. You will be analyzing a user-uploaded resume that has been converted to plain text. You also have already provided some feedback on the resume. Your job is to answer any questions the user has about their resume or the feedback provided",
-      },
-      // {
-      //   role: "system",
-      //   content: `resume: ${resume?.text}`,
-      // },
-      // {
-      //   role: "system",
-      //   content: `feedback you have already provided. Use it as context for responding to any questions users have: ${feedbacks
-      //     .map((f: Feedback, index) => {
-      //       return `\n- ${f.title}: ${f.text ?? ""} \n${f.actionableFeedbacks
-      //         ?.map(
-      //           (aF, aFIndex) =>
-      //             `${aFIndex + 1}. ${aF.title}: ${aF.text ?? ""}`
-      //         )
-      //         .join("\n")}`;
-      //     })
-      //     .join("")}`,
-      // },
-    ],
-    resumeId: resume?.id,
-    applicationId: resume?.applicationId,
-    userId: resume?.userId,
-  },
-});
-return {chat}
-}
-
-const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
-  const [resume, setResume] = useState<Resume | null>(null);
-  const [status, setStatus] = useState<
-    "Done" | "Loading" | "Analyzing" | "Analyzed" | "Saving to database"
-  >("Loading");
-  const [sortBy, setSortBy] = useState<string>("");
-  const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
-  return (
-    <LayoutContext.Provider
-      value={{ sortBy, setSortBy, status, setStatus, resume, setResume, feedbacks, setFeedbacks }}
-    >
-      {children}
-    </LayoutContext.Provider>
-  );
-};
-
-/*
-
-LEGACY CODE
-
-const ChangeTitle = ({title}: {title: string}) => {
-  const {setTitle} = useContext(context.resume.LayoutContext)
-
-  useEffect(() => {
-    setTitle(title)
-  }, [title, setTitle]);
-
-  return null;
-}
-
-*/
-
-export { LayoutContext, LayoutProvider };
+// export { LayoutContext, LayoutProvider };
