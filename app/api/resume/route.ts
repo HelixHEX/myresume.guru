e7e94be86ba8a667d147f94ddd94ba02cf1295a4
Elias Wambugu <e.wambugu192@gmail.com>
2025-03-19 12:41:08 -0700

    manually configure trigger.dev

--- app/api/resume/route.ts
+++ app/api/resume/route.ts
@@ -1,7 +1,13 @@
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { NextResponse, type NextRequest } from "next/server";
-import { tasks } from "@trigger.dev/sdk/v3";
+import { tasks, configure } from "@trigger.dev/sdk/v3";
+
+configure({
+  // this is the default and if the `TRIGGER_SECRET_KEY` environment variable is set, can omit calling configure
+  secretKey: process.env.TRIGGER_SECRET_KEY,
+});
+
 export async function GET({ }: NextRequest) {
   const user = await currentUser();
 
