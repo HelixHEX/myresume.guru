e43ef7580835a78a9a2c6f93593b0c9d33f04fe6
Elias Wambugu <e.wambugu192@gmail.com>
2025-03-25 02:34:00 -0700

    minor changes

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -17,7 +17,6 @@ import {
 	SidebarMenuSubButton,
 	SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
-import UpgradeBtn from "./upgrade-btn";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
