c45a715636a8d50e4fd8081c85b724eced9a3c3f
Elias Wambugu <e.wambugu192@gmail.com>
2025-03-28 11:29:39 -0700

    added try catch

--- app/api/ai/chat/route.ts
+++ app/api/ai/chat/route.ts
@@ -2,75 +2,94 @@ import { openai } from "@ai-sdk/openai";
 import { appendClientMessage, appendResponseMessages, streamText } from "ai";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { LanguageModelV1Prompt } from "ai";
+import { addMemories } from "@mem0/vercel-ai-provider";
+import { createMem0 } from "@mem0/vercel-ai-provider";
+
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { userId } = await auth();
-  const { messages, chatId } = await req.json();
+  try {
+    const { userId } = await auth();
+    const { messages, chatId } = await req.json();
 
-  const prevMessages = await prisma.message.findMany({
-    where: {
-      chatId,
-    },
-  })
+    const prevMessages = await prisma.message.findMany({
+      where: {
+        chatId,
+      },
+    })
 
-  const resume = await prisma.resume.findUnique({
-    where: {
-      id: chatId,
-    },
-    include: {
-      improvements: true,
-    },
-  })
+    const resume = await prisma.resume.findUnique({
+      where: {
+        id: chatId,
+      },
+      include: {
+        improvements: true,
+      },
+    })
 
-  const formattedPrevMessages = prevMessages.map((message) => ({
-    id: message.id,
-    role: message.role as "user" | "assistant" | "system",
-    content: message.content,
-    createdAt: message.createdAt,
-  }))
+    if (!resume) {
+      return new Response("Resume not found", { status: 404 })
+    }
 
-  const message = messages[messages.length - 1]
-
-  const newMessages = appendClientMessage({
-    messages, message: {
+    const formattedPrevMessages = prevMessages.map((message) => ({
       id: message.id,
       role: message.role as "user" | "assistant" | "system",
       content: message.content,
       createdAt: message.createdAt,
-    }
-  });
+    }))
 
-  await prisma.message.create({
-    data: {
-      content: message.content[0].text,
-      role: message.role,
-      userId: userId!,
-      chatId,
-    },
-  });
+    const message = messages[messages.length - 1]
 
-  const result = await streamText({
-    model: openai("gpt-4o-mini"),
-    messages: [{
-      role: "system",
-      content: `You are a resume guru assistant. Here is the resume text and improvements that have already been recommended. Resume: ${resume?.text} Improvements: ${resume?.improvements?.map((improvement) => `${improvement.title}: ${improvement.text}`).join(", ")}`
-    }, ...formattedPrevMessages, ...newMessages],
-    async onFinish({ response, text }) {
-      await appendResponseMessages({
-        messages,
-        responseMessages: response.messages,
-      })
-      await prisma.message.create({
-        data: {
-          content: text,
-          role: "assistant",
-          userId: userId!,
-          chatId
-        },
-      });
-    }
-  });
+    const newMessages = appendClientMessage({
+      messages, message: {
+        id: message.id,
+        role: message.role as "user" | "assistant" | "system",
+        content: message.content,
+        createdAt: message.createdAt,
+      }
+    });
 
-  return result.toDataStreamResponse();
+    await prisma.message.create({
+      data: {
+        content: message.content[0].text,
+        role: message.role,
+        //biome-ignore lint:
+        userId: userId!,
+        chatId,
+      },
+    });
+
+    const { userId: clerkId, fileKey, status, text, analysis, candidateName, candidateEmail, candidatePhone, candidateLocation, technicalSkills, companies, jobTitles, education, ...resumeData } = resume
+
+    const result = await streamText({
+      model: openai("gpt-4o-mini"),
+      messages: [{
+        role: "system",
+        content: `You are a resume guru assistant. Here is the resume text and improvements that have already been recommended. Resume in JSON format: ${resumeData} Improvements generated: ${resume?.improvements?.map((improvement) => `${improvement.title}: ${improvement.text}`).join(", ")}`
+      }, ...formattedPrevMessages, ...newMessages],
+      async onFinish({ response, text }) {
+        await appendResponseMessages({
+          messages,
+          responseMessages: response.messages,
+        })
+        await prisma.message.create({
+          data: {
+            content: text,
+            role: "assistant",
+            //biome-ignore lint:
+            userId: userId!,
+            chatId
+          },
+        });
+
+
+      }
+    });
+
+    return result.toDataStreamResponse();
+  } catch (error) {
+    console.log(error);
+    return new Response("Error", { status: 500 });
+  }
 }
